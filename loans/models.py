a174c011d64ab7d5758fa66067c7b2b1ae74606f
Rohit Agarwal <mindprince@gmail.com>
2011-10-26 23:12:42 +0530

    Added some code, changed some settings. Project not in working state right now.

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+
+class Customers(models.Model):
+  name = models.CharField(max_length=50)
+
+class Loans(models.Model):
+  category = models.CharField(max_length=10)
+  principal = models.DecimalField(max_digits=15, decimal_places=2)
+  interestRate = models.DecimalField(max_digits=6, decimal_places=2)
+  amountPaid = models.DecimalField(max_digits=15, decimal_places=2)
+  dateTaken = models.DateTimeField(auto_now_add=True)
+  customer = models.ForeignKey(Customers)
+
+class FloatingLoans(Loans):
+  minDate = models.DateTimeField()
+  maxDate = models.DateTimeField()
+
+class FixedLoans(Loans):
+  lastInstallmentDate = models.DateTimeField()
+
+class Installments(models.Model):
+  amount = models.DecimalField(max_digits=15, decimal_places=2)
+  loan = models.ForeignKey(Loans)
+
+class PaidInstallments(Installments):
+  datePaid = models.DateTimeField()
+  merchantUsed = models.CharField(max_length=20)
+
+class DueInstallments(Installments):
+  startDate =  models.DateTimeField()
+  endDate = models.DateTimeField()
+
+class Applications(models.Model):
+  customer = models.ForeignKey(Customers)
+  loan = models.ForeignKey(Loans)
+  dateApplied = models.DateTimeField(auto_now_add=True)
+  details = models.TextField()
+  status = models.CharField(max_length=20)
+  remark = models.TextField()
+
+class SupportTickets(models.Model):
+  loan = models.ForeignKey(Loans)
+  complaintType = models.CharField(max_length=20)
+  complaintMessage = models.TextField()
+
+#class Merchants(models.Model):
+
+
+#class IdentityModule(models.Model):
+
+
+#class AccountsModule(models.Model):
+
+
+#class TransactionsModule(models.Model):
+
--- settings.py
+++ settings.py
@@ -9,8 +9,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'sqlite.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -21,7 +21,7 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Kolkata'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -58,9 +58,9 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+#    'django.middleware.common.CommonMiddleware',
+#    'django.contrib.sessions.middleware.SessionMiddleware',
+#    'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
 ROOT_URLCONF = 'loansProject.urls'
@@ -72,8 +72,9 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
+#    'django.contrib.auth',
+#    'django.contrib.contenttypes',
+#    'django.contrib.sessions',
+#    'django.contrib.sites',
+    'loansProject.loans',
 )
