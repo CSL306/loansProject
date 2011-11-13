1541e75a716fc1841dba8a9db6535222b1cf9178
Ishan Chhabra <ishan.chhabra@gmail.com>
2011-11-13 18:55:26 +0530

    Fixed settings.py. Modified  loans/admin.py to include all classes in models.py

--- loans/admin.py
+++ loans/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from loansProject.loans.models import *
 
-admin.site.register(Customers)
-admin.site.register(Loans)
-admin.site.register(PaidInstallments)
-admin.site.register(Applications)
-admin.site.register(SupportTickets)
+admin.site.register(Customer)
+admin.site.register(Loan)
+admin.site.register(ActiveLoan)
+admin.site.register(CompletedLoan)
+admin.site.register(Payment)
+admin.site.register(OverdueInstallment)
+admin.site.register(Application)
+admin.site.register(AlottedApplication)
+admin.site.register(ActiveApplication)
+admin.site.register(SupportTicket)
--- settings.py
+++ settings.py
@@ -58,9 +58,9 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-#    'django.middleware.common.CommonMiddleware',
-#    'django.contrib.sessions.middleware.SessionMiddleware',
-#    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
 ROOT_URLCONF = 'loansProject.urls'
